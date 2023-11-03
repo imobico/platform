6aac1cdaa334dbc4570bf389c7e7ac7c6997b889
Kaue Machado <kaumac@gmail.com>
2023-11-03 13:02:35 -0300

    wip

--- src/app/onboarding/layout.tsx
+++ src/app/onboarding/layout.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 import { ProvidersWrapper } from '@/providers'
-import { Box, Center, Flex, H1, Text } from '@/ui'
+import { Box, Center, Flex, H2, Text } from '@/ui'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -19,25 +19,26 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               style={{ width: '100%', height: 'auto' }}
             />
           </Box>
-          <H1 fontSize="4xl" color="trusty.1" mb="4" fontWeight="bolder">
+          <H2 fontSize="4xl" color="trusty.1" mb="4" fontWeight="bolder">
             Bem-vindo à imobi!
-          </H1>
+          </H2>
           <Text fontSize="xl" color="trusty.2" mb="6" textAlign="center" letterSpacing="-0.5px">
             Já está tudo certo com a sua conta, agora só precisamos saber um pouco mais sobre você e
             o seu negócio.
           </Text>
         </Center>
-        <Box
+        <Center
           borderTopLeftRadius="3xl"
           borderBottomLeftRadius="3xl"
           bg="white"
           flex="1"
           boxShadow="0 0 60px 6px token(colors.trusty.10), 0 0 8px 4px token(colors.trusty.10)"
+          flexDirection="column"
         >
           <Box px="12" py="8">
             {children}
           </Box>
-        </Box>
+        </Center>
       </Flex>
     </ProvidersWrapper>
   )
