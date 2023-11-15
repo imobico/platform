68efad989c5265c01373fba4fc9c002597f69a97
Kaue Machado <kaumac@gmail.com>
2023-11-15 12:29:31 -0300

    wip landing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -6,12 +6,13 @@ export default function Home() {
   return (
     <Center width="100vw" flexDirection="column">
       <Center mt="24" flexDirection="column">
-        <H1 fontSize="5xl" textAlign="center" lineHeight="1.2" mb="8" maxWidth="640px">
-          <Strong color="trusty.8">Gerencie seus imóveis</Strong> com muito mais eficiência
+        <H1 fontSize="6xl" textAlign="center" lineHeight="1.2" mb="8" maxWidth="720px">
+          Tecnologia com <Strong color="trusty.8">resultados</Strong> e{' '}
+          <Strong color="trusty.8">eficiência</Strong> para imobiliárias e corretores.
         </H1>
-        <Text textAlign="center" fontSize="xl" color="slate.11" maxWidth="720px">
-          A imobi traz a mais moderna tecnologia para aumentar em 300% a eficiência de corretores e
-          imobiliárias na gestão de seus negócios
+        <Text textAlign="center" fontSize="2xl" color="slate.11" maxWidth="540px">
+          Através das mais recentes tecnologias automatizamos tarefas e aumentamos a eficiência de
+          imobiliárias e corretores <Strong>em até 180%</Strong>.
         </Text>
       </Center>
     </Center>
