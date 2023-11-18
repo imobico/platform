e7c33e5cb2a62f4f9499402e25daab82ea58970c
Kaue Machado <kaumac@gmail.com>
2023-11-17 21:58:19 -0300

    oops

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+// import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 // import { NextRequest, NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
 
