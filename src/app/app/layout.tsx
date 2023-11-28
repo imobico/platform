0023c0cf3268723ad3c7029b8950611b7d3ee87c
Kaue Machado <kaumac@gmail.com>
2023-11-28 19:36:30 -0300

    wip navbar

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -9,34 +9,33 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <ProvidersWrapper>
       <Flex minHeight="100vh" minWidth="100vw" bg="white">
         <Box
-          width={{ base: '80px', '2xl': '280px' }}
+          width={{ base: '82px', '2xl': '280px' }}
           bg="linear-gradient(0deg, rgba(252,253,254,1) 0%, rgba(239,243,247,1) 100%)"
         >
           <Box
             position="absolute"
             top={0}
             left={0}
-            width={{ base: '80px', '2xl': '280px' }}
+            width={{ base: '82px', '2xl': '280px' }}
             height="100vh"
             overflow="hidden"
             bg="linear-gradient(0deg, rgba(252,253,254,1) 0%, rgba(239,243,247,1) 100%)"
             _hover={{
-              width: { base: '80px', md: '280px' }
+              width: { base: '82px', md: '280px' }
             }}
             transition="all ease-in-out 0.2s"
             borderRight="1px solid token(colors.slate.5)"
           >
-            <Box position="absolute" top={4} left="20px" width="136px">
+            <Box position="absolute" top="9px" left="12px" width="136px">
               <Image
                 priority
-                src="/logo-imoblr-light-bg.svg"
+                src="/imoblr-app-navbar-logo.svg"
                 alt="Imobi Test"
                 width={240}
                 height={0}
                 sizes="100vw"
                 style={{ width: '100%', height: 'auto' }}
               />
-              <Box>osdfks</Box>
             </Box>
           </Box>
 
