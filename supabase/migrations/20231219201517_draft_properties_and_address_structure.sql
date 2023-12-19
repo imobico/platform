92aa186dcf000b1fa499e41e9daed8ddf12ff8ce
Kaue Machado <kaumac@gmail.com>
2023-12-19 17:15:37 -0300

    wip address

---
+++ supabase/migrations/20231219201517_draft_properties_and_address_structure.sql
@@ -0,0 +1,24 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_property_address()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$begin
+  RAISE LOG 'property address: %', new;
+  RAISE LOG 'address id: %', new.id;
+  RAISE LOG 'address property id: %', new.property;
+
+  update properties
+  set address = new.id
+  where id = new.property;
+
+  return new;
+end;
+
+
+
+$function$
+;
+
+
