d71a0b84f130b1ff7ed8d238946908e657ae4cc8
Kaue Machado <kaumac@gmail.com>
2023-12-24 14:19:09 -0300

    ooops

--- src/ui/Input/index.tsx
+++ src/ui/Input/index.tsx
@@ -1,5 +1,8 @@
 import { type HTMLStyledProps, styled } from '@/styled'
 import { baseInput } from '@/styled/recipes'
 
+export const Input = styled('input', baseInput)
+export type InputProps = HTMLStyledProps<typeof Input>
+
 export const BaseInput = styled('input', baseInput)
 export type BaseInputProps = HTMLStyledProps<typeof BaseInput>
